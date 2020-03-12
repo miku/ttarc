4fb91aa58ac48df3a9b9d0c464f883645d193e15
Martin Czygan <martin.czygan@gmail.com>
2020-03-12 21:37:16 +0100

    a basic version

--- cmd/ttarc/main.go
+++ cmd/ttarc/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/sethgrid/pester"
+)
+
 // Trending was generated via https://github.com/bemasher/JSONGen from
 // https://m.tiktok.com/node/share/trending.
 type Trending struct {
@@ -20,8 +36,8 @@ type Trending struct {
 					InteractionType struct {
 						Type string `json:"@type"`
 					} `json:"interactionType"`
-					Type                 string `json:"@type"`
-					UserInteractionCount int64  `json:"userInteractionCount"`
+					Type                 string      `json:"@type"`
+					UserInteractionCount interface{} `json:"userInteractionCount"`
 				} `json:"interactionStatistic"`
 				Name string `json:"name"`
 				Type string `json:"@type"`
@@ -35,8 +51,8 @@ type Trending struct {
 				InteractionType struct {
 					Type string `json:"@type"`
 				} `json:"interactionType"`
-				Type                 string `json:"@type"`
-				UserInteractionCount int64  `json:"userInteractionCount"`
+				Type                 string      `json:"@type"`
+				UserInteractionCount interface{} `json:"userInteractionCount"`
 			} `json:"interactionStatistic"`
 			Keywords     string   `json:"keywords"`
 			Name         string   `json:"name"`
@@ -50,6 +66,72 @@ type Trending struct {
 	StatusCode int64  `json:"statusCode"`
 }
 
-func main() {
+var (
+	directoryPrefix = flag.String("P", ".", "output directory")
+	warcName        = flag.String("f", fmt.Sprintf("ttarc-%s", time.Now().Format("20060102150405")), "basename for warc file")
+	userAgent       = flag.String("ua", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36", "user agent")
+	verbose         = flag.Bool("verbose", false, "be verbose")
+)
 
+func main() {
+	flag.Parse()
+	trending := "https://m.tiktok.com/node/share/trending"
+	req, err := http.NewRequest("GET", trending, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("User-Agent", *userAgent)
+	resp, err := pester.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var (
+		buf     bytes.Buffer
+		payload Trending
+	)
+	r := io.TeeReader(resp.Body, &buf)
+	dec := json.NewDecoder(r)
+	if err := dec.Decode(&payload); err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := ioutil.TempFile("", "ttarc-tmp-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Printf("close failed: %v", err)
+		}
+		if err := os.Remove(f.Name()); err != nil {
+			log.Printf("cleanup failed: %v", err)
+		}
+	}()
+	if _, err := fmt.Fprintln(f, trending); err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range payload.Body.ItemList {
+		if _, err := fmt.Fprintln(f, item.ContentUrl); err != nil {
+			log.Fatal(err)
+		}
+	}
+	cmd := exec.Command("wget",
+		"--directory-prefix", *directoryPrefix,
+		"--waitretry", "60",
+		"--random-wait",
+		"--warc-file", *warcName,
+		"--warc-cdx", *warcName,
+		"--input-file", f.Name())
+	if *verbose {
+		log.Println(cmd)
+	}
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *verbose {
+		log.Println(string(b))
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/miku/ttarc
+
+go 1.13
+
+require github.com/sethgrid/pester v0.0.0-20190127155807-68a33a018ad0
